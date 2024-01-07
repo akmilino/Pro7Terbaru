39be5b5175787ef90f6ca557e6c439db62c00641
akmilino <ak.milino20@gmail.com>
2024-01-07 23:34:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ model_path = "model_efficient_pneumonia.h5"
 loaded_model = tf.keras.models.load_model((model_path),
                                           custom_objects={'KerasLayer': hub.KerasLayer}, compile=False)
 
-# Fungsi untuk memproses gambar input
 def preprocess_image(img_path):
     img = image.load_img(img_path, target_size=(224, 224))
     img_array = image.img_to_array(img)
@@ -18,7 +17,6 @@ def preprocess_image(img_path):
     img_array = preprocess_input(img_array)
     return img_array
 
-# Aplikasi Streamlit
 selected_page = st.sidebar.radio("Menu", ["Home", "Prediction", "About Us"])
 
 if selected_page == "Home":
@@ -29,22 +27,17 @@ if selected_page == "Home":
 elif selected_page == "Prediction":
     st.title("Upload Gambar")
 
-    # Mengunggah gambar melalui Streamlit
     uploaded_file = st.file_uploader("Pilih gambar...", type=["jpg", "png", "jpeg"])
 
     if uploaded_file is not None:
-        # Menampilkan gambar yang diunggah
         st.image(uploaded_file, caption="Gambar Berhasil Diunggah.", use_column_width=True)
 
-        # Menambahkan tombol prediksi
         if st.button("Prediksi"):
-            # Memproses gambar dan membuat prediksi
             img_array = preprocess_image(uploaded_file)
             predictions = loaded_model.predict(img_array)
 
-            # Menampilkan prediksi
             st.subheader("Prediksi:")
-            class_names = ["negative", "severe"]  # Ganti dengan nama kelas yang sesuai
+            class_names = ["negative", "severe"]  
             predicted_class = class_names[np.argmax(predictions)]
 
             st.write(f"Hasil Prediksi: {predicted_class}")
